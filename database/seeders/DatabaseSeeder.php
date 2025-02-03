d83b942068be713bd65f9df9102ec730621db29a
Harry.kouevi <harry.kouevi@gmaol.com>
2025-02-03 14:34:27 +0000

    run role seed

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * File name: DatabaseSeeder.php
+ * Last modified: 2024.04.18 at 17:53:53
+ * Author: SmarterVision - https://codecanyon.net/user/smartervision
+ * Copyright (c) 2024
+ */
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB ;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $this->call(RolesTableSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+}
