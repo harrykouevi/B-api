52e79a8d40c9ccad0809e9acbecb32beeec05cc1
Harry.kouevi <harry.kouevi@gmaol.com>
2025-03-06 18:47:57 +0000

    create wallet at register

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -45,7 +45,7 @@ class PaymentService
     public function createPayment(float $amount)
     {
         $wallet = $this->walletRepository->findByField('user_id',  auth()->id());
-        if(empty($wallet)){
+        if($wallet->empty()){
             $wallet = $this->createWallet(auth()->id() , 0) ;
         }
 
