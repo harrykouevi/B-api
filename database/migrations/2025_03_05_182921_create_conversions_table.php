6e02de8cf419c567a502cc968d5744246225b41a
Harry.kouevi <harry.kouevi@gmaol.com>
2025-03-06 13:28:19 +0000

    create wallet at register

--- database/migrations/2025_03_05_182921_create_conversions_table.php
+++ database/migrations/2025_03_05_182921_create_conversions_table.php
@@ -11,11 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('conversions', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-
+      
         Schema::create('conversions', function (Blueprint $table) {
             $table->id();
             $table->string('status')->nullable();
