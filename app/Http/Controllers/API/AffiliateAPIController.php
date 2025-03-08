06bcbcad37e4931f16ff9fca2ae2ff57f77daec7
Harry.kouevi <harry.kouevi@gmaol.com>
2025-03-08 20:06:59 +0000

    mis à jour de l'utilisateur sponsorisé

--- app/Http/Controllers/API/AffiliateAPIController.php
+++ app/Http/Controllers/API/AffiliateAPIController.php
@@ -202,6 +202,7 @@ class AffiliateAPIController extends Controller
         try {
             if (auth()->user()->sponsorship_at )  return $this->sendError("already get sponsored",404);
             $affiliation =$this->affiliateRepository->findByField('code',$affiliationCode_)->first();
+            dump($affiliation) ;
             
             if ($affiliation == Null )  return $this->sendError("unprocessable partenership",404);
             if (auth()->id() == $affiliation->user_id) return $this->sendError("unprocessable partenership",404);
