5d7d4d0134cc24ead62e2954644712a052ec4b9d
Harry.kouevi <harry.kouevi@gmaol.com>
2025-03-17 17:47:57 +0000

    .

--- app/Http/Controllers/API/SalonOwner/UserAPIController.php
+++ app/Http/Controllers/API/SalonOwner/UserAPIController.php
@@ -132,7 +132,7 @@ class UserAPIController extends Controller
             $defaultRoles = $this->roleRepository->findByField('name', 'salon owner');
             $defaultRoles = $defaultRoles->pluck('name')->toArray();
             $user->assignRole($defaultRoles);
-            // $user = $user->fresh();
+            $user = $user->fresh();
 
             if ($user->sponsorship_at == Null && $request->has('code_affiliation') ) { 
                 $affiliation = $this->partenerShipService->find($request->input('code_affiliation')) ;
