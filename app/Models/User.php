11534e46336bb83fc0379a223a8a7269103109c6
Harry.kouevi <harry.kouevi@gmaol.com>
2025-04-03 18:13:16 +0000

    03/04/2025:added email otp verifications:1

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable implements HasMedia
     public static array $rules_v2 = [
         'name' => 'required|string|max:255',
         'email' => 'nullable|string|max:255|unique:users|required_unless:phone_number,null',
-        'password' => 'required|string|min:5|confirmed',
+        'password' => 'nullable|string|min:5|confirmed',
         'phone_number' => [
             'nullable',
             'max:255', 
