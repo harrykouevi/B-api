5c55d80644ebc058496f88781f09e4d42f75ad23
Harry.kouevi <harry.kouevi@gmaol.com>
2025-04-04 22:22:48 +0000

    04/04/2025:at user update user , added bio and addresse:1

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -36,7 +36,7 @@ class UpdateUserRequest extends FormRequest
         if ($this->has('device_token')) {
             return ['device_token' => 'required|max:255'];
         }
-        User::$rules['email'] = 'nullable|max:255|email|unique:users,email,' . $this->route('user');
+        User::$rules['email'] = 'nullable|max:255|email|required_without:phone_numbe|unique:users,email,' . $this->route('user');
         User::$rules['phone_number'] = 'nullable|max:255|unique:users,phone_number,' . $this->route('user');
         User::$rules['password'] = 'nullable';
         return User::$rules;
