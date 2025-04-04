7617a8ef07ab2ea6211828bdedd2e90f5134fc1b
Harry.kouevi <harry.kouevi@gmaol.com>
2025-04-04 22:05:19 +0000

    04/04/2025:at user update do not required email and phone number:1

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -36,8 +36,8 @@ class UpdateUserRequest extends FormRequest
         if ($this->has('device_token')) {
             return ['device_token' => 'required|max:255'];
         }
-        User::$rules['email'] = 'required|max:255|email|unique:users,email,' . $this->route('user');
-        User::$rules['phone_number'] = 'required|max:255|unique:users,phone_number,' . $this->route('user');
+        User::$rules['email'] = 'nullable|max:255|email|unique:users,email,' . $this->route('user');
+        User::$rules['phone_number'] = 'nullable|max:255|unique:users,phone_number,' . $this->route('user');
         User::$rules['password'] = 'nullable';
         return User::$rules;
     }
