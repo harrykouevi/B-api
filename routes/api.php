71b65272b012172e13c265fb03754ed9533bb8ce
urbain <urbainbalogou19@gmail.com>
2025-09-14 09:34:28 +0000

    maj

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\WalletAPIController;
 use App\Http\Controllers\API\PaymentAPIController;
 use App\Http\Controllers\API\UploadAPIController;
-use App\Http\Controllers\API\CinetpayTransferController;
 
 
 /*
