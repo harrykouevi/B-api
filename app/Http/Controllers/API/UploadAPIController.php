9f8180865da432dcd59f5b20c866fcf5e262ab71
Harry.kouevi <harry.kouevi@gmaol.com>
2025-11-04 12:46:23 +0000

    deleting image using id

--- app/Http/Controllers/API/UploadAPIController.php
+++ app/Http/Controllers/API/UploadAPIController.php
@@ -76,11 +76,13 @@ class UploadAPIController extends Controller
 
             // Si c'est un format UUID standard
             if (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i', $identifier)) {
-                // $upload = Upload::where('uuid', $identifier)->first();
-                $upload = Upload::where('id', $identifier)->first();
+                $upload = Upload::where('uuid', $identifier)->first();
+                // $upload = Upload::where('id', $identifier)->first();
             }
             // Si c'est un ID numérique
             else if (is_numeric($identifier)) {
+                Log::info('Upload deletion with id : ' . $identifier);
+
                 $upload = Upload::find($identifier);
             }
             // Si c'est une URL, essayer d'extraire l'UUID ou l'ID
---
+++ tests/Feature/UploadDeleteTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Media;
+use App\Models\Upload;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+
+
+class UploadDeleteTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        try{
+            Log::info(Upload::all() ) ;
+
+            $user2 = User::create([
+                'name' => 'userddEdd test',
+                'email' => 'user2E82@example.com',
+                'phone_number' => '+00228900409988',
+                'phone_verified_at' => now(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('password125'),
+                'api_token' => Str::random(60),
+                'device_token' => '',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+            $user2->assignRole(3);
+            $response =  $this->actingAs($user2, 'api')->postJson('http://localhost/api/uploads/clear', [
+                    "uuid" => 1
+                ]);
+        
+            Log::info(Upload::all() ) ;
+            
+            $response->assertStatus(200);
+        } catch (\Exception $e) {
+            Log::error('FAIL:'. $e->getMessage() , [
+                 'trace' => $e->getTraceAsString()
+            ]);
+        }
+    }
+}
