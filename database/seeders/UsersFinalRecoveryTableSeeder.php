29d33f0c060ab0d7276fa707b982cf8027a98cb7
Harry.kouevi <harry.kouevi@gmaol.com>
2025-11-07 14:04:59 +0000

    restore finals users

--- database/seeders/UsersFinalRecoveryTableSeeder.php
+++ database/seeders/UsersFinalRecoveryTableSeeder.php
@@ -18,10 +18,10 @@ class UsersFinalRecoveryTableSeeder extends Seeder
     {
          // Disable foreign key checks
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            // (239, "EUGÈNE CUTS", NULL, "+22871840739", NULL, NULL, "'.Hash::make('CHARM1234').'", NULL, "", NULL, "'.now().'", "'.now().'", NULL, NULL, NULL, NULL, NULL, NULL),
 
         DB::statement('
             INSERT INTO `users` (`id`, `name`, `email`, `phone_number`, `phone_verified_at`, `email_verified_at`, `password`, `api_token`, `device_token`, `remember_token`, `created_at`, `updated_at`, `sponsorship_at`, `sponsorship`, `otp`, `otp_expires_at`, `address`, `bio`) VALUES
-            (239, "EUGÈNE CUTS", NULL, "+22871840739", NULL, NULL, "'.Hash::make('CHARM1234').'", NULL, "", NULL, "'.now().'", "'.now().'", NULL, NULL, NULL, NULL, NULL, NULL),
             (245, "La’Klinik - Barber Shop", NULL, "+22890895555", NULL, NULL, "'.Hash::make('CHARM1234').'", NULL, "", NULL, "'.now().'", "'.now().'", NULL, NULL, NULL, NULL, NULL, NULL),
             (248, "I GOT FRESH", NULL, "+22892353460", NULL, NULL, "'.Hash::make('CHARM1234').'", NULL, "", NULL, "'.now().'", "'.now().'", NULL, NULL, NULL, NULL, NULL, NULL),
             (251, "VIVI COIFFURE", NULL, "+22899185571", NULL, NULL, "'.Hash::make('CHARM1234').'", NULL, "", NULL, "'.now().'", "'.now().'", NULL, NULL, NULL, NULL, NULL, NULL),
