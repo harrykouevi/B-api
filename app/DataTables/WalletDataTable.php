aaf4afad9e9ffa9170229b702c5914d617b23d43
Harry.kouevi <harry.kouevi@gmaol.com>
2025-12-15 15:41:34 +0000

    feat: correction list des wallets

--- app/DataTables/WalletDataTable.php
+++ app/DataTables/WalletDataTable.php
@@ -85,11 +85,11 @@ class WalletDataTable extends DataTable
                 'name' => 'currency',
                 'title' => trans('lang.wallet_currency'),
             ],
-            (auth()->check() && auth()->user()->hasRole('admin')) ? [
+            [
                 'name' => 'user.name',
                 'title' => trans('lang.wallet_user_id'),
 
-            ] : null,
+            ],
             [
                 'data' => 'enabled',
                 'title' => trans('lang.wallet_enabled'),
