6dcd7fe9b05d814f84094fa29af90ab8b81a68e0
Harry.kouevi <harry.kouevi@gmaol.com>
2025-12-15 17:30:53 +0000

    feat: correction list des wallets

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -127,7 +127,7 @@
 @endif
 <!-- Submit Field -->
 <div class="form-group col-12 d-flex flex-column flex-md-row justify-content-md-end justify-content-sm-center border-top pt-4">
-    @if($id !== 1 )
+    @if(isset($user) && $user->id !== 1 )
     
     <button type="submit" class="btn bg-{{setting('theme_color')}} mx-md-3 my-lg-0 my-xl-0 my-md-0 my-2">
         <i class="fas fa-save"></i> {{trans('lang.save')}} {{trans('lang.user')}}</button>
