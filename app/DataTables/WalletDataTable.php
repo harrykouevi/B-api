a565dfe2540d13e808b1f619fd598250ea2471b0
Harry.kouevi <harry.kouevi@gmaol.com>
2025-12-15 15:36:50 +0000

    feat: correction list des wallets

--- app/DataTables/WalletDataTable.php
+++ app/DataTables/WalletDataTable.php
@@ -34,7 +34,7 @@ class WalletDataTable extends DataTable
     {
         $wallets = $query->with(['user'])->get();
 
-    dd($wallets); // 🔴 STOP ici, tu vois TOUT
+    dd($wallets->pluck('user')); // 🔴 STOP ici, tu vois TOUT
         $dataTable = new EloquentDataTable($query);
         $columns = array_column($this->getColumns(), 'data');
         return $dataTable
