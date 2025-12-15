a12eb923c0bccb74112954df156df20759a47a47
Harry.kouevi <harry.kouevi@gmaol.com>
2025-12-15 15:19:44 +0000

    feat: correction list des wallets

--- app/DataTables/WalletDataTable.php
+++ app/DataTables/WalletDataTable.php
@@ -33,8 +33,8 @@ class WalletDataTable extends DataTable
     public function dataTable(mixed $query): DataTableAbstract
     {
         $dataTable = new EloquentDataTable($query);
+        dd($query->get()) ;
         $columns = array_column($this->getColumns(), 'data');
-
         return $dataTable
             ->editColumn('updated_at', function ($wallet) {
                 return getDateColumn($wallet, 'updated_at');
@@ -45,19 +45,19 @@ class WalletDataTable extends DataTable
             ->editColumn('balance', function ($wallet) {
                 return getPriceColumn($wallet, 'balance', $wallet->currency);
             })
-            // ->editColumn('currency.name', function ($wallet) {
-            //     if (isset($wallet->currency)) {
-            //         return $wallet->currency->name;
-            //     } else {
-            //         return "";
-            //     }
-            // })
-            // ->editColumn('user.name', function ($wallet) {
-            //     if (!isset($wallet->user)) {
-            //         return ''; 
-            //     }
-            //     return getLinksColumnByRouteName([$wallet->user], 'users.edit', 'id', 'name');
-            // })
+            ->editColumn('currency.name', function ($wallet) {
+                if (isset($wallet->currency)) {
+                    return $wallet->currency->name;
+                } else {
+                    return "";
+                }
+            })
+            ->editColumn('user.name', function ($wallet) {
+                if (!isset($wallet->user)) {
+                    return ''; 
+                }
+                return getLinksColumnByRouteName([$wallet->user], 'users.edit', 'id', 'name');
+            })
             ->addColumn('action', 'wallets.datatables_actions')
             ->rawColumns(array_merge($columns, ['action']));
     }
@@ -70,26 +70,26 @@ class WalletDataTable extends DataTable
     protected function getColumns(): array
     {
         $columns = [
-            // [
-            //     'data' => 'name',
-            //     'title' => trans('lang.wallet_name'),
+            [
+                'data' => 'name',
+                'title' => trans('lang.wallet_name'),
 
-            // ],
+            ],
             [
                 'data' => 'balance',
                 'title' => trans('lang.wallet_balance'),
 
             ],
-            // [
-            //     'data' => 'currency.name',
-            //     'name' => 'currency',
-            //     'title' => trans('lang.wallet_currency'),
-            // ],
-            // (auth()->check() && auth()->user()->hasRole('admin')) ? [
-            //     'data' => 'user.name',
-            //     'title' => trans('lang.wallet_user_id'),
+            [
+                'data' => 'currency.name',
+                'name' => 'currency',
+                'title' => trans('lang.wallet_currency'),
+            ],
+            (auth()->check() && auth()->user()->hasRole('admin')) ? [
+                'data' => 'user.name',
+                'title' => trans('lang.wallet_user_id'),
 
-            // ] : null,
+            ] : null,
             [
                 'data' => 'enabled',
                 'title' => trans('lang.wallet_enabled'),
@@ -101,19 +101,19 @@ class WalletDataTable extends DataTable
                 'searchable' => false,
             ]
         ];
-        // $columns = array_filter($columns);
-        // $hasCustomField = in_array(Wallet::class, setting('custom_field_models', []));
-        // if ($hasCustomField) {
-        //     $customFieldsCollection = CustomField::where('custom_field_model', Wallet::class)->where('in_table', '=', true)->get();
-        //     foreach ($customFieldsCollection as $key => $field) {
-        //         array_splice($columns, $field->order - 1, 0, [[
-        //             'data' => 'custom_fields.' . $field->name . '.view',
-        //             'title' => trans('lang.wallet_' . $field->name),
-        //             'orderable' => false,
-        //             'searchable' => false,
-        //         ]]);
-        //     }
-        // }
+        $columns = array_filter($columns);
+        $hasCustomField = in_array(Wallet::class, setting('custom_field_models', []));
+        if ($hasCustomField) {
+            $customFieldsCollection = CustomField::where('custom_field_model', Wallet::class)->where('in_table', '=', true)->get();
+            foreach ($customFieldsCollection as $key => $field) {
+                array_splice($columns, $field->order - 1, 0, [[
+                    'data' => 'custom_fields.' . $field->name . '.view',
+                    'title' => trans('lang.wallet_' . $field->name),
+                    'orderable' => false,
+                    'searchable' => false,
+                ]]);
+            }
+        }
         return $columns;
     }
 
