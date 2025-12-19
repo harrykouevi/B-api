eae42516af7a5475912ce465033cde7c1f93d8bd
Harry.kouevi <harry.kouevi@gmaol.com>
2025-12-19 12:32:13 +0000

    restaure a payment to

--- tests/Feature/createInternalPaymentTest.php
+++ tests/Feature/createInternalPaymentTest.php
@@ -25,20 +25,20 @@ class createInternalPaymentTest extends TestCase
             $receiver = User::find(29) ;
             $purchase = Purchase::find(38) ;
 
-            // $purchasepayment = app(PaymentService::class)->createPayment(1000,$clientW ,$receiver,Null,$purchase->taxes);
-            // $purchasepayment = $purchasepayment[0];
+            $purchasepayment = app(PaymentService::class)->createPayment(1000,$clientW ,$receiver,Null,$purchase->taxes);
+            $purchasepayment = $purchasepayment[0];
 
             
-            dd( [
-                $clientW->toArray(),
-                $receiver->toArray(),
-                $purchase->toArray(),
-                // 'payment_id' => $purchasepayment ? $purchasepayment->id : 'NULL'
-            ]);
-            // if($purchasepayment){
-            //     //mise à jour du purchase comme étant payé et validé
-            //     $purchase = app(PurchaseRepository::class)->update(['payment_id' => $purchasepayment->id , 'purchase_status_id' => 2  ], $purchase->id);
-            // }
+            // dd( [
+            //     $clientW->toArray(),
+            //     $receiver->toArray(),
+            //     $purchase->toArray(),
+            //     // 'payment_id' => $purchasepayment ? $purchasepayment->id : 'NULL'
+            // ]);
+            if($purchasepayment){
+                //mise à jour du purchase comme étant payé et validé
+                $purchase = app(PurchaseRepository::class)->update(['payment_id' => $purchasepayment->id , 'purchase_status_id' => 2  ], $purchase->id);
+            }
 
         } catch (\Exception $e) {
             Log::error('FAIL:'. $e->getMessage() , [
