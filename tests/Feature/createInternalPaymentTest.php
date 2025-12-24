1fcbccadf78dee0b8afcb818ef52174d4af5810e
Harry.kouevi <harry.kouevi@gmaol.com>
2025-12-24 12:11:25 +0000

    .

--- tests/Feature/createInternalPaymentTest.php
+++ tests/Feature/createInternalPaymentTest.php
@@ -26,20 +26,21 @@ class createInternalPaymentTest extends TestCase
            //315
            //333
             //313
+            //311
             $payer = setting('app_default_wallet_id');
             $receiver = User::find(311) ;
             $amount = 10000 ;
             // $purchase = Purchase::find(38) ;
 
-            $payment = app(PaymentService::class)->createPayment($amount,$payer ,$receiver,  WalletType::PRINCIPAL);
-            $payment__ = $payment[0];
+            // $payment = app(PaymentService::class)->createPayment($amount,$payer ,$receiver,  WalletType::PRINCIPAL);
+            // $payment__ = $payment[0];
 
             
             dd( [
                 $payer,
                 $receiver->toArray(),
                 WalletType::PRINCIPAL ,
-                $payment__->toArray(),
+                // $payment__->toArray(),
                 // 'payment_id' => $purchasepayment ? $purchasepayment->id : 'NULL'
             ]);
             // if($purchasepayment){
