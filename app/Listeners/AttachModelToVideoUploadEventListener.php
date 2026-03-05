22e01d1f1e9ec32903b9b72683124bc1a53eb606
Harry.kouevi <harry.kouevi@gmaol.com>
2026-03-05 11:39:34 +0000

    feat: finalize with cloudflare storage:4

--- app/Listeners/AttachModelToVideoUploadEventListener.php
+++ app/Listeners/AttachModelToVideoUploadEventListener.php
@@ -26,26 +26,26 @@ class AttachModelToVideoUploadEventListener
      */
     public function handle(object $event ): void
     {    
-        AttachModelToVideoUploaded::dispatch($event->upload_uuid ,$event->model);
+        // AttachModelToVideoUploaded::dispatch($event->upload_uuid ,$event->model);
  
-        // $cacheUpload = $this->uploadRepository->getByUuid($event->upload_uuid);
+        $cacheUpload = $this->uploadRepository->getByUuid($event->upload_uuid);
        
-        // $mediaItem = null;
-        // $attempts = 0;
-        // $maxAttempts = 10;
+        $mediaItem = null;
+        $attempts = 0;
+        $maxAttempts = 10;
 
-        // while ($mediaItem === null ) {
-        //     $mediaItem = $cacheUpload->getMedia('*')->first();
+        while ($mediaItem === null ) {
+            $mediaItem = $cacheUpload->getMedia('*')->first();
 
-        //     if ($mediaItem === null) {
-        //         sleep(5); // attendre 1 seconde
+            if ($mediaItem === null) {
+                sleep(5); // attendre 1 seconde
                 
-        //     }
-        // }
+            }
+        }
 
-        // if ($mediaItem) {
-        //     $mediaItem->copy($event->model, $event->collection, $event->disk);
-        // }
+        if ($mediaItem) {
+            $mediaItem->copy($event->model, $event->collection, $event->disk);
+        }
     
     }
 }
