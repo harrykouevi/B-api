8107c88eb96f1dc9b960b07bb3bb63df8c052892
Harry.kouevi <harry.kouevi@gmaol.com>
2026-04-17 14:38:27 +0000

    find better way to send otp

--- app/Services/InfoBipService.php
+++ app/Services/InfoBipService.php
@@ -86,7 +86,7 @@ class InfoBipService{
         $phone = $this->refactorPhoneNumber($phone);
 
         $body = [
-            "applicationId"=>  "47E15114840A4BC670CDA974DCD3B84B" ,
+            "application_id"=>  "47E15114840A4BC670CDA974DCD3B84B" ,
 
             'messages' => [
                 [
