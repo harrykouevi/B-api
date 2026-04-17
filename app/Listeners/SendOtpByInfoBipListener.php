d48d8842359b7e84c9eca875fd0d5ca3dcd5fa11
Harry.kouevi <harry.kouevi@gmaol.com>
2026-04-17 14:53:24 +0000

    find better way to send otp

--- app/Listeners/SendOtpByInfoBipListener.php
+++ app/Listeners/SendOtpByInfoBipListener.php
@@ -113,7 +113,7 @@ class SendOtpByInfoBipListener
     private function byWhasapp() : \Illuminate\Http\Client\Response
     {
         $data = [
-            "application_id"=>  "charm-whatsapp" ,
+            "applicationId"=>  "charm-whatsapp" ,
             "from"=> $this->_whatsappSender,
             "to"=> $this->phoneNumber,
             "content" => ['text' => "Votre code de vérification est: $this->code . Ce code est à usage unique et expirera prochainement."]
