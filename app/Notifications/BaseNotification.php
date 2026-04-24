8600d34e6a1da71545457d0c841a0b61289bbaf9
Harry.kouevi <harry.kouevi@gmaol.com>
2026-04-24 00:05:05 +0000

    hh

--- app/Notifications/BaseNotification.php
+++ app/Notifications/BaseNotification.php
@@ -62,8 +62,8 @@ abstract class BaseNotification extends Notification
         $baseData = $this->getBaseData($notifiable, $data);
         
         // $message->content($notification)->priority(FcmMessage::PRIORITY_HIGH);
-        // $message->data($baseData)->priority(FcmMessage::PRIORITY_HIGH);
-        $message->content($notification)->data($baseData)->priority(FcmMessage::PRIORITY_HIGH);
+        $message->data($baseData)->priority(FcmMessage::PRIORITY_HIGH);
+        // $message->content($notification)->data($baseData)->priority(FcmMessage::PRIORITY_HIGH);
 
         if ($to = $notifiable->routeNotificationFor('fcm', $this)) {
             $message->to($to);
